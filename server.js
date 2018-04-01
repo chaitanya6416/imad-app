39244045450087680a3ea4e29c87d76a85b12c0a
chaitanya6416 <36512605+chaitanya6416@users.noreply.github.com>
2018-04-01 14:57:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,12 +127,12 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
+/*
 app.get('/:articleName', function(req,res){
 var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
-
+*/
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
