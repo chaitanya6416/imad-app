4269322d47d5b3e2021497b8495f32a0ec803083
chaitanya6416 <36512605+chaitanya6416@users.noreply.github.com>
2018-04-01 14:37:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,27 +1,8 @@
 var button = document.getElementById('counter');
 button.onclick = function(){
   
-  //create a request boject
-  var request = new XMLHttpRequest();
+ 
+ 
   
-  //capture the response and store it in a variable
-  request.onreadystatechange =function(){
-    if(request.readyState === XMLHttpRequest.DONE){
-        //take some action
-        if(request.status === 200){
-            
-            var counter= request.responseText;
-            var span=document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-        
-        
-    }
-      
-  };
-  
-  //make a request
-  request.open=('GET','http://katakam6416.imad.hasura-app.io/counter',true);
-  request.send(null);
   
 };
